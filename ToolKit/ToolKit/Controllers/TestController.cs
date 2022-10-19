32ad75039af7afb7eb9f8be50c6707ba0beab9e6
hss <18754956388@163.com>
2022-10-19 11:55:54 +0800

    【搭建】配置Api相关控制器与路由功能

---
+++ ToolKit/ToolKit/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToolKit.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult QueryString()
+        {
+            return Ok("这是一个GET");
+        }
+    }
+}
--- ToolKit/ToolKit/Program.cs
+++ ToolKit/ToolKit/Program.cs
@@ -5,9 +5,28 @@ namespace ToolKit
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            #region 添加服务
+            // 添加对控制器和与 API 相关的功能，但不是视图或页面的支持
+            builder.Services.AddControllers();
+            #endregion
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            #region 配置管道
+            // 判断当前运行环境
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            // 添加了对属性路由的控制器支持
+            app.MapControllers();
+
+            app.UseRouting();
+            // 添加跨域
+            app.UseCors();
+
+            #endregion
 
             app.Run();
         }
